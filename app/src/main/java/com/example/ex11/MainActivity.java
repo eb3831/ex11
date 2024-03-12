755163512308a34a625caf59020fd91dc3508559
eb3831 <eb3831@bs.amalnet.k12.il>
2024-03-12 09:52:00 +0200

    end

--- app/src/main/java/com/example/ex11/MainActivity.java
+++ app/src/main/java/com/example/ex11/MainActivity.java
@@ -8,7 +8,6 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
